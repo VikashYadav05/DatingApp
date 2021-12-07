9453689693ebd45a3655e6a973aef7bdbd8f7580
VikashYadav05 <vy58442@gmail.com>
2021-12-07 10:44:25 +0530

    User Controller Added

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+public class DataContext : DbContext
+{
+	public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+       
+    } 
+   public  DbSet<AppUser> Users { get; set; }
+}
